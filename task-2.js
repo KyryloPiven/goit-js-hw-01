14c413102bc031dae49e0ce7c36da8a591670377
Kyrylo Piven <pivenkirill24@gmail.com>
2024-08-18 22:59:54 +0100

    1saveHWJS

--- task-2.js
+++ task-2.js
@@ -1,6 +1,7 @@
-document.querySelector(`.out-1`).addEventListener(`mousemove`, blockWidth);
-let w = 100;
-function blockWidth() {
-  w++;
-  document.querySelector(`.out-1`).style.width = w + `px`;
+function getShippingMessage(country, price, deliveryFee) {
+  const totalPrice = price + deliveryFee;
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
+console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
--- task-3.js
+++ task-3.js
@@ -1,7 +1,11 @@
-const courseTopic = "JavaScript essentials";
-const courseTopicLength = courseTopic;
-const firstElement = courseTopic;
-const lastElement = courseTopic;
-console.log(courseTopicLength.length);
-console.log(firstElement[0]);
-console.log(lastElement[lastElement.length - 1]);
+function getElementWidth(content, padding, border) {
+  const contentWidth = parseFloat(content);
+  const paddingtWidth = parseFloat(padding) * 2;
+  const bordertWidth = parseFloat(border) * 2;
+  const totalWidth = contentWidth + paddingtWidth + bordertWidth;
+
+  return totalWidth;
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
